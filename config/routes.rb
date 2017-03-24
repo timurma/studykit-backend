bc6540abb4f942fbf6cf49bef3148eaf18b799f7
Timur Platonov <S.R.mmlacc@outlook.com>
2017-03-24 12:31:48 +0300

    introduce admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
     resources :courses, only: %i(index show create) do
       resources :lectures, only: %i(index)
     end
+
+    namespace :admin do
+      resources :users, only: %i(create) do
+        post 'login', on: :collection, to: 'sessions#create'
+      end
+    end
   end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
