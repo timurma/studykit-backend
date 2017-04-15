abdb9ee863e7a550d2d55b6cb5937583a18494ed
Timur Platonov <S.R.mmlacc@outlook.com>
2017-04-15 08:56:17 +0300

    temporary fix for course content

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,10 +1,13 @@
 class CourseSerializer < ActiveModel::Serializer
   belongs_to :owner
-  has_many :content
 
-  attributes :id, :title, :description, :avatar, :owner
+  attributes :id, :title, :description, :avatar, :owner, :content
 
   def avatar
     "#{instance_options[:host]}#{object.avatar.url}" if object.avatar?
+  end
+
+  def content
+    object.content.map(&:specific)
   end
 end
