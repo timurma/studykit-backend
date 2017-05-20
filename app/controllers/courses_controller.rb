a65779fe81ae678cc4e063991860ee7445f578a4
Timur Platonov <S.R.mmlacc@outlook.com>
2017-05-20 17:42:00 +0300

    add courses# update/destroy for admins/teachers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show]
+  before_action :set_course, only: %i(show update destroy)
   before_action :try_set_owner, only: [:index]
-  before_action :authenticate_with_token!, only: %i(create update)
+  before_action :authenticate_with_token!, only: %i(create update destroy)
 
   def_param_group :course do
     param :course, Hash do
@@ -57,6 +57,7 @@ class CoursesController < ApplicationController
     "errors": "You are not authorized to access this page."
   }
   '
+  error code: 400, desc: 'Empty course object'
   error code: 403, desc: 'User does not have access to create courses (ex: student)'
   error code: 422, desc: 'Invalid course'
   def create
@@ -69,6 +70,56 @@ class CoursesController < ApplicationController
     else
       render json: { errors: course.errors }, status: :unprocessable_entity
     end
+  end
+
+  api!
+  param_group :course
+  example '
+  {
+    "course":{
+      "description": "my cool description"
+    }
+  }
+  {
+    "id": 7,
+    "title": "nice course",
+    "description": "my cool description",
+    "avatar": null,
+    "owner": {
+      "id": 3,
+      "firstName": "tim2",
+      "lastName": "plat",
+      "email": "tpltn",
+      "avatar": null,
+      "role": "admin"
+    },
+    "lectures": []
+  }
+  '
+  error code: 400, desc: 'Empty course object'
+  error code: 403, desc: 'User does not have access to update this course'
+  error code: 422, desc: 'Invalid course'
+  def update
+    authorize!(:update, @course)
+    if @course.update(course_params)
+      render json: @course, host: request.base_url
+    else
+      render json: { errors: @course.errors }, status: :unprocessable_entity
+    end
+  end
+
+  api!
+  example '
+  {
+    "errors": "You are not authorized to access this page."
+  }
+  '
+  error code: 403, desc: 'User does not have access to delete this course'
+  error code: 404, desc: 'Course not found'
+  def destroy
+    authorize!(:destroy, @course)
+    @course.destroy
+    head :no_content
   end
 
   private
