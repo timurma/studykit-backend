7f4a1eb42b53a4af1bc69b2a20f6e216c6305db9
Timur Platonov <S.R.mmlacc@outlook.com>
2017-05-29 17:41:20 +0300

    add wikidata items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518051725) do
+ActiveRecord::Schema.define(version: 20170529143925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,13 @@ ActiveRecord::Schema.define(version: 20170518051725) do
 
   create_table "video_contents", force: :cascade do |t|
     t.string "url"
+  end
+
+  create_table "wikidata_items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "wikidata_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_foreign_key "course_categories", "categories"
