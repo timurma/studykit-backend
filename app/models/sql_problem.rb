eb0675df458a93c9a1692412fc50cac2362d44ff
Timur Platonov <S.R.mmlacc@outlook.com>
2017-06-02 23:03:28 +0300

    fix executable validation

--- app/models/sql_problem.rb
+++ app/models/sql_problem.rb
@@ -1,5 +1,6 @@
 class SqlProblem < ApplicationRecord
   has_many :lecture_content
 
-  validates_presence_of :solution_code, :check_function, :executable
+  validates_presence_of :solution_code, :check_function
+  validates_inclusion_of :executable, in: [true, false]
 end
