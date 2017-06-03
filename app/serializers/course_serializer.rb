25c5ab1263f95ba045c29196b62d7b59ae0c3cb4
Timur Platonov <S.R.mmlacc@outlook.com>
2017-06-03 18:15:54 +0300

    add created_at to course serializer

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -2,9 +2,13 @@ class CourseSerializer < ActiveModel::Serializer
   belongs_to :owner
   has_many :lectures
 
-  attributes :id, :title, :description, :avatar, :owner
+  attributes :id, :title, :description, :avatar, :owner, :created_at
 
   def avatar
     "#{instance_options[:host]}#{object.avatar.url}" if object.avatar?
+  end
+
+  def created_at
+    object.created_at.to_s(:db)
   end
 end
