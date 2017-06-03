f6b8bba5dafd6152688fa25b83e312b7abccb5d0
Timur Platonov <S.R.mmlacc@outlook.com>
2017-06-03 20:01:00 +0300

    render lecture_content with course

--- app/serializers/lecture_serializer.rb
+++ app/serializers/lecture_serializer.rb
@@ -1,7 +1,5 @@
 class LectureSerializer < ActiveModel::Serializer
-  has_many :content
-
-  attributes :id, :title, :serial_number, :course_id
+  attributes :id, :title, :serial_number, :course_id, :content
 
   def content
     collection = object.content.map(&:specific)
--- app/serializers/sql_problem_content_serializer.rb
+++ app/serializers/sql_problem_content_serializer.rb
@@ -1,9 +1,9 @@
 class SqlProblemContentSerializer < LectureContentSerializer
-  attributes :body, :title, :sql_problem_id, :sql_solutions
+  attributes :body, :title, :sql_problem_id #, :sql_solutions
 
-  def sql_solutions
-    collection = object.sql_solutions
-    ActiveModel::Serializer::CollectionSerializer.new(collection,
-                                                      each_serializer: SqlSolutionSerializer)
-  end
+  # def sql_solutions
+  #   collection = object.sql_solutions
+  #   ActiveModel::Serializer::CollectionSerializer.new(collection,
+  #                                                     each_serializer: SqlSolutionSerializer)
+  # end
 end
