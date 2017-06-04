3f0039eeee88515d93db4625ffdc645eec6cd42a
Timur Platonov <S.R.mmlacc@outlook.com>
2017-06-04 15:31:39 +0300

    fx lecture serialization

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,9 +1,8 @@
 class CourseSerializer < BaseCourseSerializer
   belongs_to :owner
+  has_many :lectures
 
   # TODO: remove god-like course object, which includes lectures, lecture_contents and etc.
-  attributes :owner, :lectures
-
   def lectures
     object.lectures.order(:serial_number)
   end
